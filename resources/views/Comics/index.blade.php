98aac14771cb827d28467d9711a4d5affa8611d5
GiovanniCalia <johnca@outlook.it>
2022-05-10 00:29:39 +0200

    Completamento dell'esercizio con stilizzazione tramite sass

--- resources/views/Comics/index.blade.php
+++ resources/views/Comics/index.blade.php
@@ -5,17 +5,15 @@
 <main>
     <ol class="d-flex flex-wrap justify-content-between">
         @foreach ($comics as $comic)
-         <li class="text-center">
-             <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
-             <h3><a href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a></h3>
-             <p>{{ $comic->description }}</p>
-             <div>{{ $comic->price }}</div>
-             <div>{{ $comic->series }}</div>
-             <div>{{ $comic->sale_date }}</div>
-             <div>{{ $comic->tipe }}</div>
+             <li class="text-center">
+                 <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                 <h3><a href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a></h3>
+                 <div>{{ $comic->price }}</div>
+                 <div>{{ $comic->sale_date }}</div>
+                 <div>{{ $comic->type }}</div>
             </li>   
          @endforeach
      </ol>
      
-     {{ $comics->links() }}
+    {{ $comics->links() }}
 </main>
--- resources/views/Comics/show.blade.php
+++ resources/views/Comics/show.blade.php
@@ -3,6 +3,16 @@
 @section('title', 'Comic')
 
 @section('main')
-<h1>{{ $comic->title }}</h1>
-<p>{{ $comic->description }}</p>
+<main class="main_box">
+<div class="text-center box">
+    <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+    <h1>{{ $comic->title }}</h1>
+    <p>{{ $comic->description }}</p>
+    <h3>From the series: {{ $comic->series }}</h3>
+    <div>Price: <strong>{{ $comic->price }}</strong></div>
+    <div>Sale date: {{ $comic->sale_date }}</div>
+    <div>{{ $comic->type }}</div>
+</div>
+</main>
+
 @endsection
